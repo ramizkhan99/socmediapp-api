2ddc22c428084f23eb76fb449234e6447e6b8cd7
Ramiz Khan <ramizkhan1199@gmail.com>
2020-08-29 19:47:18 +0530

    Fixed user function error

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,39 +1,53 @@
-import mongoose from 'mongoose';
-import uniqueValidator from 'mongoose-unique-validator';
-import crypto from 'crypto'
-import { Binary, Timestamp, ObjectID } from 'mongodb';
-import { timeStamp } from 'console';
+import mongoose from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+import crypto, { BinaryLike } from "crypto";
+import { Binary, Timestamp, ObjectID } from "mongodb";
 
-var UserSchema = new mongoose.Schema({
-    username:{type:String,unique:true,required:[true,"can't be blank"],match: [/^[a-zA-Z0-9]+$/, 'is invalid'],index:true},
-    email:{type:String,unique:true,required:[true,"can't be blank"],match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-    avatar:Binary,
-    hash:String,
-    salt:String,
-    createDate:Timestamp,
-    lastLogin:Timestamp,
-    location:String,
-    active:Boolean,
-    posts:[{ type:ObjectID }],
-    likedPost:[{type:ObjectID}]
-    
-},
-{
-    timestamps:true
-})
+var UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+      index: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, "is invalid"],
+      index: true,
+    },
+    avatar: Binary,
+    hash: String,
+    salt: String,
+    createDate: Timestamp,
+    lastLogin: Timestamp,
+    location: String,
+    active: Boolean,
+    posts: [{ type: ObjectID }],
+    likedPost: [{ type: ObjectID }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+UserSchema.plugin(uniqueValidator, { message: "is already taken." });
 
-UserSchema.methods.setPassword = password =>{
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password,this.salt,10000,512,'sha512').toString('hex');
-}
+UserSchema.methods.setPassword = function (password: BinaryLike) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex");
+};
 
-UserSchema.methods.validPassword = password =>{
-    var hash = crypto.pbkdf2Sync(password,this.salt,10000,512,'sha512').toString('hex');
-    return this.hash === hash;
-}
+UserSchema.methods.validPassword = function (password: BinaryLike) {
+  var hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex");
+  return this.hash === hash;
+};
 
-export default mongoose.model('User', UserSchema);
-
-
+export default mongoose.model("User", UserSchema);
