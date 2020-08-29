1a854d201b8afa028b332ca9c2a4d2aa9b5cb975
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2020-08-29 18:34:58 +0530

    added user schema

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
 import express from "express";
+import User from './models/user'
 require("./models/mongoose");
 export const app = express();
---
+++ src/models/user.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+import crypto from 'crypto'
+import { Binary, Timestamp, ObjectID } from 'mongodb';
+import { timeStamp } from 'console';
+
+var UserSchema = new mongoose.Schema({
+    username:{type:String,unique:true,required:[true,"can't be blank"],match: [/^[a-zA-Z0-9]+$/, 'is invalid'],index:true},
+    email:{type:String,unique:true,required:[true,"can't be blank"],match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+    avatar:Binary,
+    hash:String,
+    salt:String,
+    createDate:Timestamp,
+    lastLogin:Timestamp,
+    location:String,
+    active:Boolean,
+    posts:[{ type:ObjectID }],
+    likedPost:[{type:ObjectID}]
+    
+},
+{
+    timestamps:true
+})
+
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+
+UserSchema.methods.setPassword = password =>{
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password,this.salt,10000,512,'sha512').toString('hex');
+}
+
+UserSchema.methods.validPassword = password =>{
+    var hash = crypto.pbkdf2Sync(password,this.salt,10000,512,'sha512').toString('hex');
+    return this.hash === hash;
+}
+
+export default mongoose.model('User', UserSchema);
+
+
