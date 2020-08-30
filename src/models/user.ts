f12cbc8e764d84e7b3c49dd73bc781b0233b622e
Ramiz Khan <ramizkhan1199@gmail.com>
2020-08-30 19:14:17 +0530

    Formatting changes

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,50 +4,50 @@ import crypto, { BinaryLike } from "crypto";
 import { Binary, Timestamp, ObjectID } from "mongodb";
 
 var UserSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      unique: true,
-      required: [true, "can't be blank"],
-      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
-      index: true,
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: [true, "can't be blank"],
+            match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+            index: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, "can't be blank"],
+            match: [/\S+@\S+\.\S+/, "is invalid"],
+            index: true,
+        },
+        avatar: Binary,
+        hash: String,
+        salt: String,
+        createDate: Timestamp,
+        lastLogin: Timestamp,
+        location: String,
+        active: Boolean,
+        posts: [{ type: ObjectID }],
+        likedPost: [{ type: ObjectID }],
     },
-    email: {
-      type: String,
-      unique: true,
-      required: [true, "can't be blank"],
-      match: [/\S+@\S+\.\S+/, "is invalid"],
-      index: true,
-    },
-    avatar: Binary,
-    hash: String,
-    salt: String,
-    createDate: Timestamp,
-    lastLogin: Timestamp,
-    location: String,
-    active: Boolean,
-    posts: [{ type: ObjectID }],
-    likedPost: [{ type: ObjectID }],
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 UserSchema.plugin(uniqueValidator, { message: "is already taken." });
 
 UserSchema.methods.setPassword = function (password: BinaryLike) {
-  this.salt = crypto.randomBytes(16).toString("hex");
-  this.hash = crypto
-    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
-    .toString("hex");
+    this.salt = crypto.randomBytes(16).toString("hex");
+    this.hash = crypto
+        .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+        .toString("hex");
 };
 
 UserSchema.methods.validPassword = function (password: BinaryLike) {
-  var hash = crypto
-    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
-    .toString("hex");
-  return this.hash === hash;
+    var hash = crypto
+        .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+        .toString("hex");
+    return this.hash === hash;
 };
 
 export default mongoose.model("User", UserSchema);
