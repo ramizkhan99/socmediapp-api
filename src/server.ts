3fd4c01edbb8d38e01212c071795ce70de7c7dd1
Ramiz Khan <ramizkhan1199@gmail.com>
2020-09-21 10:35:29 +0530

    Changed default port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import { app } from './app';
+import { app } from "./app";
 
-const PORT = process.env.PORT || '3000';
+const PORT = process.env.PORT || "5000";
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("server running on port: " + PORT);
-})
+});
