5289a56afd2c1075e5566688c827765a3cb26b0b
Ramiz Khan <ramizkhan1199@gmail.com>
2020-09-29 20:23:49 +0530

    Removed non required comments

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,64 +1,61 @@
-import { Schema, Types, Document, model, Mongoose } from "mongoose";
-import { Int32, ObjectID } from "mongodb";
+import { Schema, Types, Document, model } from "mongoose";
+import { Int32 } from "mongodb";
 let PostSchema = new Schema(
     {
-        authorId:{
-            type:String,
-            required:true,
+        authorId: {
+            type: String,
+            required: true
         },
-        authorName:{
-            type:String,
-            required:true,
+        authorName: {
+            type: String,
+            required: true
         },
-        title:{
-            type:String,
-            required:true
+        title: {
+            type: String,
+            required: true
         },
-        content:{
-            type:String,
-            required:true
+        content: {
+            type: String,
+            required: true
         },
-        genre:{
-            type:String,
-            required:false,
-            default:"General"
+        genre: {
+            type: String,
+            required: false,
+            default: "General"
         },
-        comments:[{
-            author:{
-                type:String,
-                required:true
-            },
-            authorId:{
-                type:String,
-                required:true
-            },
-            content:{
-                type:String,
-                required:true
+        comments: [
+            {
+                author: {
+                    type: String,
+                    required: true
+                },
+                authorId: {
+                    type: String,
+                    required: true
+                },
+                content: {
+                    type: String,
+                    required: true
+                }
             }
-
-        }],
-        likes:{
-            type:Int32,
-
-        },
-        
-
+        ],
+        likes: {
+            type: Int32
+        }
     },
     {
-        timestamps:true
-    },
-)
+        timestamps: true
+    }
+);
 
 export interface IPostSchema extends Document {
-    authorId:String;
-    authorName:String;
-    title:String;
-    content:String;
-    genre:String;
-    comments:Types.Array<Object>;
-    likes:Int32;
-
+    authorId: String;
+    authorName: String;
+    title: String;
+    content: String;
+    genre: String;
+    comments: Types.Array<Object>;
+    likes: Int32;
 }
 
 const Post = model<IPostSchema>("Post", PostSchema);
