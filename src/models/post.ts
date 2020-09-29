b1a95bf56a8e9d379c766eee6a532c136307befb
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2020-09-29 19:29:03 +0530

    post schema added

---
+++ src/models/post.ts
@@ -0,0 +1,66 @@
+import { Schema, Types, Document, model, Mongoose } from "mongoose";
+import { Int32, ObjectID } from "mongodb";
+let PostSchema = new Schema(
+    {
+        authorId:{
+            type:String,
+            required:true,
+        },
+        authorName:{
+            type:String,
+            required:true,
+        },
+        title:{
+            type:String,
+            required:true
+        },
+        content:{
+            type:String,
+            required:true
+        },
+        genre:{
+            type:String,
+            required:false,
+            default:"General"
+        },
+        comments:[{
+            author:{
+                type:String,
+                required:true
+            },
+            authorId:{
+                type:String,
+                required:true
+            },
+            content:{
+                type:String,
+                required:true
+            }
+
+        }],
+        likes:{
+            type:Int32,
+
+        },
+        
+
+    },
+    {
+        timestamps:true
+    },
+)
+
+export interface IPostSchema extends Document {
+    authorId:String;
+    authorName:String;
+    title:String;
+    content:String;
+    genre:String;
+    comments:Types.Array<Object>;
+    likes:Int32;
+
+}
+
+const Post = model<IPostSchema>("Post", PostSchema);
+
+export default Post;
